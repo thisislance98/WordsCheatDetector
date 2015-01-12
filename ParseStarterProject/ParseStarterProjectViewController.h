a98aa2f0ea80d79314b87d8c72796db4ef2e3768
Anatoliy Kozlov <anatoliykozlov@eagles.ewu.edu>
2015-01-12 12:45:07 -0800

    Initial Commit

---
+++ ParseStarterProject/ParseStarterProjectViewController.h
@@ -0,0 +1,13 @@
+//
+//  ParseStarterProjectViewController.h
+//  ParseStarterProject
+//
+//  Copyright 2014 Parse, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+
+@interface ParseStarterProjectViewController : UIViewController
+
+@end
